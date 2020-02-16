5bb2e61ee60266273e4410e0894bb3333c9ba070
weiquanquan <ring2977@163.com>
2020-02-16 23:56:48 +0800

    加入redis完善登录功能、添加验证码功能

---
+++ src/main/java/xyz/ring2/admin/core/entity/vo/CaptchaVo.java
@@ -0,0 +1,9 @@
+package xyz.ring2.admin.core.entity.vo;
+
+/**
+ * @author :     weiquanquan
+ * @date :       2020/2/16 22:45
+ * description:
+ **/
+public class CaptchaVo {
+}
---
+++ src/main/java/xyz/ring2/admin/utils/RedisUtil.java
@@ -0,0 +1,9 @@
+package xyz.ring2.admin.utils;
+
+/**
+ * @author :     weiquanquan
+ * @date :       2020/2/16 16:54
+ * description:
+ **/
+public class RedisUtil {
+}
